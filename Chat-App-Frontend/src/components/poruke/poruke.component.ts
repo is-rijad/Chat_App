28a45d3fd250a6e9deb2b7c038074fb4a66c348b
is-rijad <isrijad@gmail.com>
2024-02-07 17:27:17 +0100

    Zahtjev 3,4 - Slanje poruka u globalnom kanalu i lista poruka (prepravka)

--- Chat-App-Frontend/src/components/poruke/poruke.component.ts
+++ Chat-App-Frontend/src/components/poruke/poruke.component.ts
@@ -3,11 +3,15 @@ import {CookieService} from "ngx-cookie-service";
 import {Konstante} from "../../helperi/konstante";
 import {SignalR} from "../../servisi/signalr";
 import {Poruka} from "../../modeli/privatna-poruka";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-poruke',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './poruke.component.html',
   styleUrl: './poruke.component.css',
   providers: [
@@ -16,12 +20,13 @@ import {Poruka} from "../../modeli/privatna-poruka";
 })
 export class PorukeComponent implements OnInit{
   korisnickoIme = "";
+  poruke : Poruka[] = [];
   constructor(private cookieService:CookieService,
               private signalR:SignalR) {
   }
   ngOnInit() {
     this.korisnickoIme = this.cookieService.get(Konstante.korisnickoIme);
-    this.signalR.konekcija.on(Konstante.primiPoruku, (poruka) => this.primiPoruku(poruka))
+    this.signalR.konekcija.on(Konstante.primiPoruku, (poruka) => this.poruke.push(poruka))
   }
 
   posaljiPoruku() {
@@ -31,25 +36,5 @@ export class PorukeComponent implements OnInit{
       sadrzaj: (document.getElementById("poruka-input") as HTMLInputElement).value
     }
     this.signalR.konekcija.invoke(Konstante.posaljiPoruku, poruka)
-  }
-  primiPoruku(poruka : Poruka) {
-    let div = document.createElement("div");
-    div.className = (poruka.odKorisnika == this.korisnickoIme) ? "korisnikova-poruka" : "poruka";
-    let h2 = document.createElement("h2");
-    h2.className = (poruka.odKorisnika == this.korisnickoIme) ? "korisnikova-poruka-korisnik" : "poruka-korisnik";
-    h2.innerHTML = poruka.odKorisnika!;
-    let textarea = document.createElement("textarea");
-    textarea.readOnly = true;
-    textarea.className = (poruka.odKorisnika == this.korisnickoIme) ? "korisnikova-poruka-text" : "poruka-text";
-    textarea.value = poruka.sadrzaj;
-    div.appendChild(h2);
-    div.appendChild(textarea);
-    document.getElementById("poruke")!.appendChild(div);
-
-      // <div class="poruka">
-      //   <h2 class="poruka-korisnik">sadsadsad</h2>
-      //   <textarea readonly class="poruka-text">${poruka.sadrzaj}</textarea>
-      // </div>
-
   }
 }
